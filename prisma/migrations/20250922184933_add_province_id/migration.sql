70a475ee9cba0c3e5b07ead1abfc825f47ac1751
rifkyr990 <rifkir802@gmail.com>
2025-09-24 13:48:45 +0700

    fix shipping again

---
+++ prisma/migrations/20250922184933_add_province_id/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "public"."Store" ADD COLUMN     "province_id" TEXT;
--- src/controllers/ShippingController.ts
+++ src/controllers/ShippingController.ts
@@ -6,16 +6,71 @@ import prisma from "../config/prisma";
 
 const RAJAONGKIR_API_KEY = process.env.RAJAONGKIR_API_KEY_COST!;
 const RAJAONGKIR_BASE_URL = "https://rajaongkir.komerce.id/api/v1/calculate/domestic-cost";
-const ORIGIN_CITY_ID = "501"; // Kota asal (misal Jakarta)
 const COURIERS = ["jne","tiki"];
 
 class ShippingController {
+  // public static getShippingOptions = asyncHandler(async (req: Request, res: Response) => {
+  //   const { addressId } = req.body;
+
+  //   if (!addressId) {
+  //     return ApiResponse.error(res, "addressId is required", 400);
+  //   }
+  //   const destinationCityId = await getCityIdFromAddress(Number(addressId));
+  //   console.log("ini destinasi", destinationCityId);
+
+  //   if (!destinationCityId) {
+  //     return ApiResponse.error(res, "City ID not found for given addressId", 404);
+  //   }
+
+  //   try {
+  //     const allOptions: any[] = [];
+
+  //     for (const courier of COURIERS) {
+  //       const params = new URLSearchParams();
+  //       params.append("origin", ORIGIN_CITY_ID);
+  //       params.append("destination", destinationCityId);
+  //       params.append("weight", "1000");
+  //       params.append("courier", courier);
+
+  //       const response = await axios.post(RAJAONGKIR_BASE_URL, params, {
+  //         headers: {
+  //           key: RAJAONGKIR_API_KEY,
+  //           "Content-Type": "application/x-www-form-urlencoded",
+  //         },
+  //       });
+
+  //       const data = response.data.data;
+
+  //       allOptions.push(
+  //         ...data.map((option: any) => ({
+  //           courier: option.name,
+  //           code: option.code,
+  //           service: option.service,
+  //           description: option.description,
+  //           cost: option.cost,
+  //           estimated: option.etd,
+  //         }))
+  //       );
+  //     }
+  //     console.log("ini berhasil", allOptions);
+  //     return ApiResponse.success(res, allOptions, "Shipping options fetched successfully");
+  //   } catch (error: any) {
+  //     console.error("RajaOngkir API error:", error?.response?.data || error.message);
+  //     return ApiResponse.error(res, "Failed to fetch shipping options", 500);
+  //   }
+  // });
   public static getShippingOptions = asyncHandler(async (req: Request, res: Response) => {
     const { addressId } = req.body;
 
     if (!addressId) {
       return ApiResponse.error(res, "addressId is required", 400);
     }
+
+    const originCityId = await getOriginCityIdFromMainStore();
+    if (!originCityId) {
+      return ApiResponse.error(res, "Origin city not found from main store", 500);
+    }
+
     const destinationCityId = await getCityIdFromAddress(Number(addressId));
     console.log("ini destinasi", destinationCityId);
 
@@ -28,7 +83,7 @@ class ShippingController {
 
       for (const courier of COURIERS) {
         const params = new URLSearchParams();
-        params.append("origin", ORIGIN_CITY_ID);
+        params.append("origin", originCityId); // GANTI DARI HARDCODE
         params.append("destination", destinationCityId);
         params.append("weight", "1000");
         params.append("courier", courier);
@@ -53,6 +108,7 @@ class ShippingController {
           }))
         );
       }
+
       console.log("ini berhasil", allOptions);
       return ApiResponse.success(res, allOptions, "Shipping options fetched successfully");
     } catch (error: any) {
@@ -62,6 +118,8 @@ class ShippingController {
   });
 }
 
+
+
 async function getCityIdFromAddress(addressId: number): Promise<string | null> {
   const address = await prisma.userAddress.findUnique({
     where: { id: addressId },
@@ -69,6 +127,14 @@ async function getCityIdFromAddress(addressId: number): Promise<string | null> {
   });
 
   return address?.city_id || null;
+}
+async function getOriginCityIdFromMainStore(): Promise<string | null> {
+  const store = await prisma.store.findFirst({
+    where: { is_main_store: true,is_active: true },
+    select: { city_id: true },
+  });
+
+  return store?.city_id || null;
 }
 
 export default ShippingController;
